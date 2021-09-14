e92c2ea781314265253d0c283ba7983d1e94984f
catalin-enache <catalinenache78@gmail.com>
2021-09-14 08:16:36 +0300

    updated index page to redirect to order-book

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,25 @@
+import { useEffect } from "react";
 import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import Layout from "../layouts/layout";
 import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/order-book");
+  }, []);
+
   return (
     <Layout>
       <Head>
         <title>Our Website</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>This is our index page</p>
+        <Link href="/order-book">
+          <a>Order Book</a>
+        </Link>
       </section>
     </Layout>
   );
